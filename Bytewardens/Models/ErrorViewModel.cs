ebc0ae3eac2701a974512ec44ff14f789c0d7c66
Osman Aslancan <osmanaslancan24@gmail.com>
2023-05-20 14:25:03 +0300

    start project

---
+++ Bytewardens/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bytewardens.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
